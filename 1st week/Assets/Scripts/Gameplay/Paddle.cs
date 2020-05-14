9ee9ab64cebda5d516d4257c3e221c6bcf0011d2
Student <mongush.baylak17@gmail.com>
2020-05-14 18:42:01 +0300

    1st week add

---
+++ 1st week/Assets/Scripts/Gameplay/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	Rigidbody2D rb;
+	float moveX;
+	Vector3 position;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		moveX = Input.GetAxis("Horizontal");
+        if (moveX != 0)
+        {
+			position = transform.position;
+			position.x += moveX * ConfigurationUtils.PaddleMoveUnitsPerSecond;
+			rb.MovePosition(position);
+        }
+	}
+}
