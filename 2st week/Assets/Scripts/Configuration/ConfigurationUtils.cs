e32e9a7d839d71477a070473b7f8b68a85464535
Student <mongush.baylak17@gmail.com>
2020-05-27 01:39:46 +0300

    add in 2nd week balls moving, spawn and few blocks

---
+++ 2st week/Assets/Scripts/Configuration/ConfigurationUtils.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Provides access to configuration data
+/// </summary>
+public static class ConfigurationUtils
+{
+    #region Properties
+
+    /// <summary>
+    /// Gets the paddle move units per second
+    /// </summary>
+    /// <value>paddle move units per second</value>
+
+    public static ConfigurationData configurationData;
+
+    public static float PaddleMoveUnitsPerSecond
+    {
+        get { return configurationData.PaddleMoveUnitsPerSecond; }
+    }
+
+    public static float BallImpulseForce
+    {
+        get { return configurationData.BallImpulseForce; }
+    }
+
+    public static float MinSpawnTime
+    {
+        get { return configurationData.MinSpawnTime; }
+    }
+
+    public static float MaxSpawnTime
+    {
+        get { return configurationData.MaxSpawnTime; }
+    }
+    #endregion
+    
+    /// <summary>
+    /// Initializes the configuration utils
+    /// </summary>
+    public static void Initialize()
+    {
+        configurationData = new ConfigurationData();
+    }
+}
---
+++ 2st week/Assets/Scripts/Gameplay/BallSpawner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject prefabBall;
+	Timer spawnTimer;
+	bool retrySpawn = false;
+	Vector2 spawnLocationMin;
+	Vector2 spawnLocationMax;
+
+	// Use this for initialization
+	void Start () {
+		GameObject tempBall = Instantiate(prefabBall, Vector3.zero, Quaternion.identity);
+		float ballColliderHalfWidth = tempBall.transform.lossyScale.x / 2;
+		float ballColliderHalfHeight = tempBall.transform.lossyScale.y / 2;
+		spawnLocationMin = new Vector2(
+			tempBall.transform.position.x - ballColliderHalfWidth,
+			tempBall.transform.position.y - ballColliderHalfHeight);
+		spawnLocationMax = new Vector2(
+			tempBall.transform.position.x + ballColliderHalfWidth,
+			tempBall.transform.position.y + ballColliderHalfHeight);
+		Destroy(tempBall);
+		spawnTimer = GetComponent<Timer>();
+		spawnTimer.Duration = 1f;
+		spawnTimer.Run();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (spawnTimer.Finished || retrySpawn)
+        {
+			SpawnBall();
+			spawnTimer.Duration = Random.Range(ConfigurationUtils.MinSpawnTime, ConfigurationUtils.MaxSpawnTime); ;
+			spawnTimer.Run();
+		}
+	}
+
+	public void SpawnBall()
+    {
+		if (Physics2D.OverlapArea(spawnLocationMin, spawnLocationMax) == null)
+		{
+			retrySpawn = false;
+			Instantiate(prefabBall);
+		}
+		else
+		{
+			retrySpawn = true;
+		}
+	}
+}
