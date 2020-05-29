9cede412cebebaa222c1632d84dfbfadb1f8d6e4
Student <mongush.baylak17@gmail.com>
2020-05-30 01:05:08 +0300

    add menu for game

---
+++ 3th week/Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MenuManager
+{
+   /* public static void GoToMenu()
+    {
+        switch (x){
+            case (MenuName.Start):
+
+                break;
+            case (MenuName.Pause):
+                break;
+            case (MenuName.Quite):
+                break;
+        }
+    }*/
+}
---
+++ 3th week/Assets/Scripts/Menu/MenuName.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// names of menu 
+/// </summary>
+
+public enum MenuName
+{
+    Start,
+    Pause,
+    Quite
+}
